47a10258d72dcb449ec7a1379558e130114ba45e
AlaminIslamOnu <alamin.ndc.17@gmail.com>
2025-02-27 08:59:18 +0600

    add 2nd page

--- script/login.js
+++ script/login.js
@@ -3,12 +3,20 @@ document.getElementById('login-btn').addEventListener('click',function(event){
   
     const accountNumber=document.getElementById('mobile-number').value;
     
+  
     
   /* const accountNumber = inputToNumber ('mobile-number'); */
-  const pin= inputToNumber('pin')
-     if (accountNumber === 11){
-          if (pin ===1234){
-              window.location.href="./main.html"
+  const pinNumber = document.getElementById('pin').value; 
+  
+   /* const pinn= inputToNumber('pin');  */
+  ;
+  
+     if (accountNumber.length === 11){
+          if (pinNumber === '1234'){
+              window.location.href="main.html"
+          }
+          else{
+            alert("Invalid pin")
           }
      }
      else {
